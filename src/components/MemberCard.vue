f2e56cef496e6985e34c0de391c144195d786023
Sameer Niaz Mohammad <niazmohsameer@gmail.com>
2020-12-13 21:25:36 +0800

    Updated MemberCard shadow

--- src/components/MemberCard.vue
+++ src/components/MemberCard.vue
@@ -27,7 +27,7 @@ export default {
 
 <style lang="scss" scoped>
 .member-card {
-  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
   border-radius: 10px;
 
   padding: $standard-padding;
