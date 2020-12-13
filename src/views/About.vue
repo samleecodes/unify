fbf60fd002457febd59bcfa223d626e4e179d44a
Sameer Niaz Mohammad <niazmohsameer@gmail.com>
2020-12-13 19:29:16 +0800

    Added intro and manifesto to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="about">
     <ImageHeader :title="$t('navbar.about')" />
+    <div class="container">
+      <article>
+        <h1>{{ $t('about.aboutTitle') }}</h1>
+        <p>{{ $t('index.intro') }}</p>
+      </article>
+      <article>
+        <h1>{{ $t('about.manifestoTitle') }}</h1>
+        <img src="../assets/Manifesto.jpeg" />
+        <p><a href="/Manifesto.jpeg">{{ $t('about.enlarge') }}</a></p>
+      </article>
+    </div>
   </div>
 </template>
 
