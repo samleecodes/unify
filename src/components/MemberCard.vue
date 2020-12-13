39a4f5a66486dd4e1d197b32b13051587a801395
Sameer Niaz Mohammad <niazmohsameer@gmail.com>
2020-12-13 21:23:44 +0800

    Implemented MemberCard and iteration

---
+++ src/components/MemberCard.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="member-card">
+    <div class="member-card-image-container">
+      <img :src="image" alt="Member Image">
+    </div>
+    <div class="member-card-content">
+      <div class="member-card-content-name">
+        {{ name }}
+      </div>
+      <div class="member-card-content-role">
+        {{ role }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MemberCard',
+  props: {
+    image: String,
+    name: String,
+    role: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.member-card {
+  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  border-radius: 10px;
+
+  padding: $standard-padding;
+
+  width: 100%;
+
+  margin-bottom: $standard-margin;
+
+  @media only screen and (min-width: 815px) {
+    display: flex;
+    flex-direction: row;
+
+    width: 48%;
+
+    .member-card-image-container {
+      margin-bottom: 0 !important;
+
+      img {
+        margin: 0 !important;
+      }
+    }
+
+    .member-card-content {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      margin-left: $standard-margin;
+    }
+  }
+
+  .member-card-image-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    img {
+      object-fit: cover;
+      width: 150px;
+      height: 150px;
+      border-radius: 50%;
+    }
+
+    margin-bottom: $standard-margin;
+  }
+
+  .member-card-content {
+    .member-card-content-name {
+      font-size: $large-font-size;
+      font-weight: 700;
+    }
+
+    .member-card-content-role {
+      font-size: $content-font-size;
+      font-weight: 400;
+    }
+  }
+}
+</style>
