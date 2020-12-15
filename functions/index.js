3e6bbc1afd3fb82c87610a1963e0da6c53b614b8
Sameer Niaz Mohammad <niazmohsameer@gmail.com>
2020-12-15 15:44:50 +0800

    Setup hello world cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,8 @@
 const functions = require('firebase-functions');
-const nodemailer = require('nodemailer');
 
-exports.onMessage = functions.firestore.document('messages/{message}').onCreate(notifyViaEmail);
+exports.helloWorld = functions.https.onRequest(helloWorldHandler);
 
-async function notifyViaEmail(snap, _context) {
-  const data = snap.data();
-
-  try {
-    const mailFrom = functions.config().mail.from;
-    const mailPwd = functions.config().mail.pwd;
-    const mailTo = functions.config().mail.to;
-
-    const mailOptions = {
-      from: mailFrom,
-      to: mailTo,
-      subject: 'New Message',
-      html: `<p>${data.message}</p>`
-    };
-
-    const transporter = nodemailer.createTransport({
-      host: 'smtp-relay.sendinblue.com',
-      port: 587,
-      auth: {
-        user: mailFrom,
-        pass: mailPwd
-      }
-    });
-
-    await transporter.sendMail(mailOptions);
-  } catch (err) {
-    console.error(err);
-  }
+async function helloWorldHandler(req, res) {
+  functions.logger.log('Hello world handler called');
+  res.send('Hello, World! ✨');
 }
